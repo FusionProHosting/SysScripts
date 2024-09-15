8dc5284908b665abd4fc6f698ecf9689702cb638
Tykari <tigerwhit0@gmail.com>
2024-09-15 09:57:46 -0500

    Create distroinstall.sh

---
+++ distroinstall.sh
@@ -0,0 +1,5 @@
+wget https://nocix.mm.fcix.net/ubuntu-releases/24.04/ubuntu-24.04.1-live-server-amd64.iso
+wget https://nocix.mm.fcix.net/ubuntu-releases/22.04/ubuntu-22.04.5-live-server-amd64.iso
+wget https://nocix.mm.fcix.net/ubuntu-releases/20.04/ubuntu-20.04.6-live-server-amd64.iso
+wget https://mirror.steadfast.net/debian-cd/current/amd64/iso-cd/debian-12.7.0-amd64-netinst.iso
+wget https://download.fedoraproject.org/pub/fedora/linux/releases/40/Server/x86_64/iso/Fedora-Server-netinst-x86_64-40-1.14.iso
